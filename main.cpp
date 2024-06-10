72c39077896660d8d267cbc9a281a557b4eafcd2
Julien Marrec <julien.marrec@gmail.com>
2024-06-10 12:56:12 +0200

    Just print, don't try to require the stdlib which I don't redistribute + remove Version 3.12 if present on mac

--- main.cpp
+++ main.cpp
@@ -9,8 +9,7 @@ int main(int argc, char* argv[]) {
   }
   Py_SetProgramName(program); /* optional but recommended */
   Py_Initialize();
-  PyRun_SimpleString("from time import time,ctime\n"
-                     "print('Today is', ctime(time()))\n");
+  PyRun_SimpleString("print('Hello from Python')");
   if (Py_FinalizeEx() < 0) {
     exit(120);
   }
